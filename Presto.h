2bf8f55b2577cc436b70f658de776a0031e0dbf4
Todd Olsen <todd.olsen@me.com>
2017-08-07 13:39:06 -0400

    Added Targets.

---
+++ Presto.h
@@ -0,0 +1,18 @@
+//
+//  Presto_iOS.h
+//  Presto-iOS
+//
+//  Created by Todd Olsen on 8/7/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Presto_iOS.
+FOUNDATION_EXPORT double Presto_iOSVersionNumber;
+
+//! Project version string for Presto_iOS.
+FOUNDATION_EXPORT const unsigned char Presto_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Presto_iOS/PublicHeader.h>
+
+
